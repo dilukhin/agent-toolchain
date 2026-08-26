0ab00c464f97d0d50497ca574d6744f3f2c88af4
Dmitry Ilukhin <7656484+dilukhin@users.noreply.github.com>
2026-08-26 12:41:16 +0300

    Make Windows setup tombstone return deterministic exit code

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -1,8 +1,9 @@
-﻿$ErrorActionPreference = "Stop"
-Write-Error @"
+$ErrorActionPreference = "Stop"
+$message = @"
 setup_windows.ps1 has been removed as a supported interface.
 Run .\bootstrap_windows.ps1 once to install toolchainctl, then use:
   toolchainctl check
   toolchainctl apply
-"@ -ErrorAction Continue
+"@
+[Console]::Error.WriteLine($message)
 exit 2
